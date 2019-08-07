1e3c555a923ce3b8cba11a90826bbb9e3d311488
johanedblom <johane92@gmail.com>
2019-08-07 15:48:19 +0200

    Add files via upload

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -17,9 +17,21 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
+	%With nested loop:
+%	num_thetas = length(theta);
+%	theta_new = zeros(num_thetas,1);
+%	for j = 1:num_thetas
+%		inner_sum = 0;
+%		for i = 1:m
+%			inner_sum = inner_sum + ((X(i,:) * theta) - y(i)) * X(i,j);
+%		end
+%		theta_new(j) = theta(j) - (alpha/m * inner_sum)
+%	end
 
-
-
+	%With vectorized approach:
+	A = X * theta - y; %(m x 1 vector)
+	delta = 1 / m * (A' * X)'; % ((n+1) x 1 vector)
+	theta = theta - (alpha*delta); % ((n+1) x 1 vector
 
 
 
